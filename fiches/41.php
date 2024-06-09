8f42b1a2b0efe4e5e5d444d987708212a8a821f4
Cyril Slucki <cslucki@gmail.com>
2024-06-09 16:48:25 +0200

    Commit du 2024-06-09 16:48:25

---
+++ fiches/41.php
@@ -0,0 +1,22 @@
+<body>
+  <h1>Isabelle Lamarque - Gérante de Société et Candidate pour le Parti Reconquête!</h1>
+
+  <h2>Parcours politique</h2>
+  <p>Isabelle Lamarque est la déléguée départementale du parti Reconquête! dans le Loiret et membre du Conseil National Reconquête!.</p>
+
+  <h2>Déclarations controversées</h2>
+  <p>Isabelle Lamarque a été au centre de controverses notamment pour ses appels à un référendum sur l'immigration. Elle a demandé aux responsables départementaux de Les Républicains (LR) et du Rassemblement National (RN) de soutenir cette initiative, soulignant que ce référendum est réclamé par une majorité de Français selon des sondages récents. Ces déclarations ont été perçues comme faisant partie de la rhétorique anti-immigration stricte du parti Reconquête! (RECONQUÊTE! Loiret - <a href="https://fede45.parti-reconquete.fr/communique-de-presse/isabelle-lamarque-deleguee-departementale-de-reconquete-demande-aux-responsables-departementaux-de-lr-et-du-rn-de-soutenir-l-initiative-de-marion-marechal-pour-imposer-un-referendum-sur-l-immigration">https://fede45.parti-reconquete.fr/communique-de-presse/isabelle-lamarque-deleguee-departementale-de-reconquete-demande-aux-responsables-departementaux-de-lr-et-du-rn-de-soutenir-l-initiative-de-marion-marechal-pour-imposer-un-referendum-sur-l-immigration</a>).</p>
+
+  <h2>Messages sur X</h2>
+  <ul>
+    <li>Réactions aux incidents et à la criminalité : Isabelle Lamarque reposte des informations sur des agressions et des attaques, exprimant son mécontentement face à la violence croissante et appelant à des mesures fermes pour assurer la sécurité publique.</li>
+    <li>Critiques politiques : Elle critique les actions et les décisions de certaines personnalités politiques, notamment envers Bruno Le Maire, dénonçant ce qu'elle considère comme une inaction ou une incompétence.</li>
+    <li>Prises de position sur des sujets sensibles : Il y a des messages dénonçant des agressions commises par des migrants, des confrontations entre groupes kurdes et turcs, ainsi que des reproches envers des élus de la France Insoumise jugés trop proches des mouvances islamistes ou pro-palestiniennes.</li>
+    <li>Soutien à Marion Maréchal : Mme Lamarque exprime son indignation face à ce qu'elle considère comme un manque de fermeté des autorités sur ces sujets. Elle appelle à voter pour le parti politique Reconquête! de Marion Maréchal.</li>
+  </ul>
+  <p>Dans l'ensemble, ces tweets véhiculent une rhétorique critique envers l'immigration, l'islam politique et la gauche radicale, reflétant les positions du parti d'extrême droite Reconquête!.</p>
+
+  <h2>Twitter</h2>
+  <p><a href="https://x.com/LamarqueIsabel2">https://x.com/LamarqueIsabel2</a></p>
+</body>
+
